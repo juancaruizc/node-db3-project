86c540dd3322116deba91b3c53456269952827ef
juancaruizc <juancruizcruz@gmail.com>
2021-03-17 21:48:01 -0700

    done

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,3 +1,5 @@
+const { OPEN_READWRITE } = require('sqlite3');
+
 /*
   If `scheme_id` does not exist in the database:
 
@@ -7,8 +9,16 @@
   }
 */
 const checkSchemeId = (req, res, next) => {
+  const { scheme_id } = req.params;
 
-}
+  if (!scheme_id) {
+    res
+      .status(404)
+      .json({ message: `scheme with scheme_id ${scheme_id} id> not found` });
+  } else {
+    next();
+  }
+};
 
 /*
   If `scheme_name` is missing, empty string or not a string:
@@ -19,8 +29,14 @@ const checkSchemeId = (req, res, next) => {
   }
 */
 const validateScheme = (req, res, next) => {
+  const { scheme_name } = req.body;
 
-}
+  if (!scheme_name || scheme_name === '' || typeof name !== 'string') {
+    res.status(400).json({ message: 'invalid scheme_name' });
+  } else {
+    next();
+  }
+};
 
 /*
   If `instructions` is missing, empty string or not a string, or
@@ -32,11 +48,20 @@ const validateScheme = (req, res, next) => {
   }
 */
 const validateStep = (req, res, next) => {
+  const instructions = req.body.instruction;
+  const steps = req.body;
 
-}
+  if (!instructions || req.body === '' || req.body !== '') {
+    res.status(400).json({ message: 'invalid step' });
+  } else if (steps === isNaN || steps < 1) {
+    res.status(400).json({ message: 'invalid step' });
+  } else {
+    next();
+  }
+};
 
 module.exports = {
   checkSchemeId,
   validateScheme,
   validateStep,
-}
+};
